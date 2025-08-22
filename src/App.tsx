f8d04acc3418629b0d7e82618f92c0ddcf84d0d8
greatjade24 <greatjade24@naver.com>
2025-08-22 13:34:25 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
