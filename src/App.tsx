855857249c26372a26e01a83b00ce44f945f0f40
greatjade24 <greatjade24@naver.com>
2025-08-26 10:56:47 +0900

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,32 @@
-// import Container from "./Container"
-// import Destination from "./Destination"
-// import Fruits from "./Fruits"
-// import Item from "./Item"
-// import Mailbox from "./Mailbox"
-import { useState } from "react"
-// import Alert from "./Alert"
-// import Form from "./Form"
-// import Propagation from "./Propagation"
-// import List, { FilteredList } from "./List"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useReducer, useState } from "react"
 
-// function HelloWorld() {
-//   return (
-//     <>
-//       <h1>Hello world!</h1>
-//     </>
-//   )
-// }
+const initial = { count: 0 }
 
-// function Message() {
-//   const name = "Taemin"
-//   return <h1>Hello {name}</h1>
-// }
-
-function Counter() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      State: {count}
-      <button
-        onClick={() => {
-          // setCount((pre) => pre + 1)
-          // setCount((pre) => pre + 1)
-          // setCount((pre) => pre + 1)
-          setCount(count + 1)
-          setTimeout(() => {
-            // 0, State는 Snapshot처럼 동작하기 때문
-            alert(count)
-          }, 1000)
-        }}
-      >
-        Update
-      </button>
-    </div>
-  )
+const reducer = (state: any, action: any) => {
+  console.log('state: ', state);
+  console.log('action: ', action);
+  
+  switch (action.type) {
+    case "INCREASE": {
+      return { count: state.count + 1 }
+    }
+    case "DECREASE": {
+      return { count: state.count - 1 }
+    }
+    default:
+      throw new Error("invalid type: " + action.type)
+  }
 }
 
 function App() {
-  // const destination = [
-  //   {
-  //     place: '강릉',
-  //     description: '재밌었던 여행'
-  //   }
-  // ]
-  // const fruits = ['바나나', '사과']
-  const [count, setCount] = useState(0)
-  const handleUpdate = () => {
-    setCount(count + 1)
-  }
-  return (
-    <div>
-      State: {count}
-      <button onClick={handleUpdate}>Update</button>
-    </div>
-  )
+  // https://ko.react.dev/reference/react/useReducer#dispatch
+  const [state, dispatch] = useReducer(reducer, initial)
+  return <div>
+    <h1>count: { state.count } { typeof dispatch }</h1>
+    <button onClick={() => dispatch({ type: "INCREASE" })}>+</button>
+    <button onClick={() => dispatch({ type: "DECREASE" })}>-</button>
+  </div>
 }
 
 export default App
