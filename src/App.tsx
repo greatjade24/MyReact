088f2bdc62541aaec1c79666d1b6639b0cd249ee
greatjade24 <greatjade24@naver.com>
2025-08-28 12:23:46 +0900

    Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useReducer, useState } from "react"
-
-const initial = { count: 0 }
-
-const reducer = (state: any, action: any) => {
-  console.log('state: ', state);
-  console.log('action: ', action);
-  
-  switch (action.type) {
-    case "INCREASE": {
-      return { count: state.count + 1 }
-    }
-    case "DECREASE": {
-      return { count: state.count - 1 }
-    }
-    default:
-      throw new Error("invalid type: " + action.type)
-  }
-}
+import { MyComponent, MyProvider } from "./MyComponent"
+import { ThemeComponent, ThemeProvider } from "./Theme"
 
 function App() {
-  // https://ko.react.dev/reference/react/useReducer#dispatch
-  const [state, dispatch] = useReducer(reducer, initial)
-  return <div>
-    <h1>count: { state.count } { typeof dispatch }</h1>
-    <button onClick={() => dispatch({ type: "INCREASE" })}>+</button>
-    <button onClick={() => dispatch({ type: "DECREASE" })}>-</button>
-  </div>
+  return (
+    <div>
+      <ThemeProvider>
+        <ThemeComponent />
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
