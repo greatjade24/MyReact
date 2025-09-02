0d08b7d79214182c0163604d0b11efdfa5a52ee5
greatjade24 <greatjade24@naver.com>
2025-09-02 13:58:11 +0900

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { MyComponent, MyProvider } from "./MyComponent"
-import { ThemeComponent, ThemeProvider } from "./Theme"
+import { useRef, useState } from "react"
 
-function App() {
+function FocusInput() {
+  const inputRef = useRef(null)
+
+  const handleClick = () => {
+    inputRef.current.focus()
+  }
   return (
     <div>
-      <ThemeProvider>
-        <ThemeComponent />
-      </ThemeProvider>
+      <input type="text" ref={inputRef} />
+      <button onClick={handleClick}>Focus</button>
+    </div>
+  )
+}
+
+function App() {
+  const [seconds, setSeconds] = useState(0)
+  const timerRef = useRef(null)
+
+  const handleStart = () => {
+    if (timerRef.current) return
+
+    timerRef.current = setInterval(() => {
+      setSeconds((pre) => pre + 1)
+    }, 1000)
+  }
+  const handleEnd = () => {
+    clearInterval(timerRef.current)
+    timerRef.current = null
+  }
+  return (
+    <div>
+      {/* <h1>Timer: {seconds}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleEnd}>End</button> */}
+      <FocusInput />
     </div>
   )
 }
