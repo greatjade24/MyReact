dba61f52d38f0cbd1f0d72cbe67ff662d50a5402
greatjade24 <greatjade24@naver.com>
2025-10-20 14:41:28 +0900

    input 필드, 추가 기능 구현

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-작업 장비 이전 테스트
-
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import Layout from "./components/Layout"
 import Title from "./components/Title"
 import Controls from "./components/Controls"
 import TodoList from "./components/TodoList"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function App() {
-  const [list, setList] = useState([{
-    id: 1,
-    text: 'hello',
-    completed: false,
-  }])
+  const idRef = useRef(0)
+  const [list, setList] = useState([])
+  const handleSubmit = (value: string) => {
+    setList((prevList) =>
+      prevList.concat({
+        id: (idRef.current += 1),
+        text: value,
+        completed: false,
+      })
+    )
+  }
   return (
     <div>
       <Layout>
         <Title />
-        <Controls />
+        <Controls onSubmit={handleSubmit} />
         <TodoList data={list} />
       </Layout>
     </div>
